35d3343018c23176b6d0148757420219c9b11075
Gianni-Rgg <gianni.ruggiero95@gmail.com>
2026-03-10 19:38:27 +0100

    ci: add workflow

---
+++ src/main/java/esiee/tp_note_tu/Tp_note_tu.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package esiee.tp_note_tu;
+
+/**
+ *
+ * @author Gianni
+ */
+public class Tp_note_tu {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
